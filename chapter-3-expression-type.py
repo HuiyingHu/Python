a608636f63442ed27cba6f783f66d995fcb26e22
Huiying Hu <huiyinghuuwa@gmail.com>
2016-07-17 21:57:18 +0800

    practice for chapter 2 & 3

---
+++ chapter-3-expression-type.py
@@ -0,0 +1,19 @@
+first = raw_input("first name:")
+middle = raw_input("middle name:")
+last = raw_input("last name:")
+
+print "vertical"
+print first
+print middle
+print last
+
+print "horizon"
+print first+middle+last
+
+print "with a space"
+print first+" "+middle+" "+last
+
+hour=raw_input("Enter the hour:")
+rate=raw_input("Enter the rate ")
+pay = float(hour) * float(rate)
+print "Pay is",pay
